80919a4da9785c1714c47cf8c9b6028146151807
Paul Grant <pgrant@KAN-D-PauGra.Savision.int>
2021-08-23 16:50:11 -0400

    Adding initial styling

--- .eleventy.js
+++ .eleventy.js
@@ -2,21 +2,29 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function (eleventyConfig) {
+  
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
-
+  
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
-
+  
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
-    );
+      );
+    });
+    
+  // SCSS Support
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: './src/_scss/main.scss',
+    outputDir: './src/static/css'
   });
-
+  
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
 
@@ -28,11 +36,10 @@ module.exports = function (eleventyConfig) {
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./src/admin/config.yml": "./admin/config.yml"
   });
+
+  // eleventyConfig.addPassthroughCopy('./src/static/css')
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
@@ -62,6 +69,7 @@ module.exports = function (eleventyConfig) {
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
   return {
+    passthroughFileCopy: true,
     dir: {
       input: "src",
     },
